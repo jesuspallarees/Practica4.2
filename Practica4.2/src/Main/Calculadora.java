7c759cd3d15f30496f44db611caf4fce7cad5940
Jesús Pallarés <jespaldia@gmail.com>
2025-05-12 09:28:20 +0200

    Añadida funcionalidad de raíz cuadrada (Soy el compañero :D)

--- Practica4.2/src/Main/Calculadora.java
+++ Practica4.2/src/Main/Calculadora.java
@@ -16,4 +16,11 @@ public class Calculadora {
     public int restar(int a, int b) {
         return a - b;
     }
+    
+    public double raizCuadrada(double a){
+        if (a < 0){
+            throw new IllegalArgumentException("No se puede calcular la raíz de un número negativo");
+        }
+        return Math.sqrt(a);
+    }
 }
